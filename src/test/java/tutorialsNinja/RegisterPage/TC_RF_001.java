4acc13f88a276840bbf239e1b49889e244ced1bb
Eshika2308 <eshika2308@gmail.com>
2025-12-07 23:03:04 +0530

    understanding on IMAP as third party protocol

--- src/test/java/tutorialsNinja/RegisterPage/TC_RF_001.java
+++ src/test/java/tutorialsNinja/RegisterPage/TC_RF_001.java
@@ -3,6 +3,7 @@ package tutorialsNinja.RegisterPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -31,6 +32,35 @@ public class TC_RF_001 {
         driver.findElement(By.xpath("//input [@type=\"checkbox\"]")).click();
         driver.findElement(By.xpath("//input [@type=\"submit\"]")).click();
 
+        //Assertions
+        Assert.assertTrue(driver.findElement(By.linkText("Logout")).isDisplayed());
+
+        String expectedTitle ="Your Account Has Been Created!";
+        Assert.assertEquals(driver.findElement(By.xpath("//div[@id='content']//h1")).getText(),expectedTitle);
+        String detailsOne ="Congratulations! Your new account has been successfully created!";
+
+        String detailsTwo="You can now take advantage of member privileges to enhance your online shopping experience with us.";
+
+        String detailsThree="If you have ANY questions about the operation of this online shop, please e-mail the store owner.";
+
+        String detailsFour="contact us";
+        String expectedProperDetails = driver.findElement(By.id("content")).getText();
+
+
+        Assert.assertTrue(expectedProperDetails.contains(detailsOne));
+        Assert.assertTrue(expectedProperDetails.contains(detailsTwo));
+        Assert.assertTrue(expectedProperDetails.contains(detailsThree));
+        Assert.assertTrue(expectedProperDetails.contains(detailsFour));
+
+
+        driver.findElement(By.xpath("//div[@class='pull-right']")).click();
+        //driver.findElement(By.xpath(//a [text ()='Continue']));
+
+        Assert.assertTrue(driver.findElement(By.xpath("//li//a[text() ='Edit your account information']")).isDisplayed());
+
+        driver.quit();
+
+
     }
 
     public String generateNewEmail() {
@@ -43,6 +73,8 @@ public class TC_RF_001 {
         //System.out.println(emailWithTimeStamp);
 
         return new Date().toString().replaceAll(":", "").replaceAll(" ", "")+"@gmail.com";
+
+
 
     }
 
---
+++ src/test/java/tutorialsNinja/RegisterPage/TC_RF_002.java
@@ -0,0 +1,12 @@
+package tutorialsNinja.RegisterPage;
+
+
+/*Email verification
+* Internet Message App protocol enabled or not, without IMAP no one can access your email
+* For third party application we need to give passcode
+*  */
+
+
+
+public class TC_RF_002 {
+}
